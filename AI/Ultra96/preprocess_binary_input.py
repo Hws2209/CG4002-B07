6a1217274859a32c1528083dea9165aa88d4dcb0
Hws2209 <wansing03@gmail.com>
2025-10-02 19:38:09 +0800

    Add isolated module for unpacking packets and preprocessing by identifier

---
+++ AI/Ultra96/preprocess_binary_input.py
@@ -0,0 +1,71 @@
+import struct
+import numpy as np
+from scipy.stats import skew
+
+# Constants
+PACKET_SIZE = 16
+NUM_SENSORS = 4
+WINDOW_SIZE = 5
+TOTAL_WINDOW_SIZE = WINDOW_SIZE * NUM_SENSORS
+CHUNK_SIZE = TOTAL_WINDOW_SIZE * PACKET_SIZE
+
+# Format string for unpacking (skip 2-byte header)
+# H = uint16, h = int16
+fmt = '<Hhhhhhh'  # '>': big-endian, '<': little-endian
+
+is_simple_mlp = False
+
+
+def extract_features(matrix):
+    features = []
+    for i in range(matrix.shape[1]): # Each axis
+        axis = matrix[:, i]
+        fft_axis = np.fft.fft(axis)
+        features.extend([
+            np.mean(axis),
+            np.std(axis),
+            np.max(axis),
+            np.min(axis),
+            np.sqrt(np.mean(axis**2)),
+            skew(axis),
+            np.max(np.abs(fft_axis)),
+            np.max(np.angle(fft_axis))
+        ])
+    return np.array(features, dtype=np.float32)
+
+def main():
+    count = 0
+    with open("packets.bin", "rb") as in_file:
+        while True:
+            # Try to read one full window worth of data
+            chunk = in_file.read(CHUNK_SIZE)
+            if not chunk or len(chunk) < CHUNK_SIZE:
+                break  # stop if file is finished
+            
+            data_window = []
+            buckets = [[] for _ in range(NUM_SENSORS)]
+
+            # Sort by identifiers
+            for i in range(TOTAL_WINDOW_SIZE):
+                start = i * PACKET_SIZE
+                end = start + PACKET_SIZE
+                packet_bytes = chunk[start:end]
+
+                # Unpack (skip first 2 bytes = header)
+                device_id, ax, ay, az, gx, gy, gz = struct.unpack(fmt, packet_bytes[2:])
+                buckets[device_id - 1].append([ax, ay, az, gx, gy, gz])
+            
+            # Form data_window
+            if is_simple_mlp:
+                data_window = [extract_features(np.array(bucket)).tolist() for bucket in buckets]
+            else:
+                data_window = [row for bucket in buckets for row in bucket]
+                
+            count += 1
+            print(f"\nNumber {count} data window:", data_window)
+            print("Size:", len(data_window), len(data_window[0]))
+            
+            #return # Only need one window for now
+
+if __name__ == "__main__":
+    main()
